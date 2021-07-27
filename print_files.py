0047508715c06605baba1be195eb7c356528c3f7
petr_shirin <p.shirin@agora.ru>
2021-07-27 12:35:16 +0800

    add script for print files

---
+++ print_files.py
@@ -0,0 +1,30 @@
+import os
+from time import sleep
+
+DIR_PATH = 'C://Users//peshi\PycharmProjects//DocxEditor//Послужные карты, для курсантов//1 взвод'
+
+if __name__ == '__main__':
+    file_names = []
+    for f in os.walk(DIR_PATH + '//Служебные характеристики'):
+        file_names = f[2]
+    for file_path in file_names:
+        os.startfile(f'{DIR_PATH}//{file_path}', 'print')
+        sleep(3)
+        os.startfile(f'{DIR_PATH}//{file_path}', 'print')
+        sleep(5)
+    file_names = []
+    for f in os.walk(DIR_PATH + '//Справки'):
+        file_names = f[2]
+    for file_path in file_names:
+        os.startfile(f'{DIR_PATH}//{file_path}', 'print')
+        sleep(3)
+        os.startfile(f'{DIR_PATH}//{file_path}', 'print')
+        sleep(3)
+        os.startfile(f'{DIR_PATH}//{file_path}', 'print')
+
+        sleep(5)
+
+
+
+
+
