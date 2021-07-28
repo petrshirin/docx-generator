f1034b3bc33d5c93d7a68b4788efd2cf3e5cf76b
petr_shirin <p.shirin@agora.ru>
2021-07-28 15:47:40 +0800

    update templates and logic

--- print_files.py
+++ print_files.py
@@ -1,7 +1,7 @@
 import os
 from time import sleep
 
-DIR_PATH = './/Послужные карты, для курсантов//1 взвод'
+DIR_PATH = 'C://Users//peshi//PycharmProjects//DocxEditor//Послужные карты, для курсантов//2 взвод'
 
 if __name__ == '__main__':
     file_names = []
@@ -10,17 +10,22 @@ if __name__ == '__main__':
     for file_path in file_names:
         if '.docx' in file_path:
             os.startfile(f'{DIR_PATH}//Служебные характеристики//{file_path}', 'print')
-            sleep(5)
+            sleep(10)
             os.startfile(f'{DIR_PATH}//Служебные характеристики//{file_path}', 'print')
-            sleep(7)
-    file_names = []
-    for f in os.walk(DIR_PATH + '//Справки'):
-        file_names = f[2]
-    for file_path in file_names:
-        if '.docx' in file_path:
-            os.startfile(f'{DIR_PATH}//Справки//{file_path}', 'print')
-            sleep(5)
-            os.startfile(f'{DIR_PATH}//Справки//{file_path}', 'print')
-            sleep(5)
-            os.startfile(f'{DIR_PATH}//Справки//{file_path}', 'print')
-            sleep(7)
+            sleep(10)
+    # for f in os.walk(DIR_PATH + '//Справки'):
+    #     file_names = f[2]
+    # for file_path in file_names:
+    #     if '.docx' in file_path:
+    #         os.startfile(f'{DIR_PATH}//Справки//{file_path}', 'print')
+    #         sleep(10)
+    #         os.startfile(f'{DIR_PATH}//Справки//{file_path}', 'print')
+    #         sleep(10)
+    #         os.startfile(f'{DIR_PATH}//Справки//{file_path}', 'print')
+    #         sleep(10)
+    # for f in os.walk(DIR_PATH + '//Аттестационные листы'):
+    #     file_names = f[2]
+    # for file_path in file_names:
+    #     if '.docx' in file_path:
+    #         os.startfile(f'{DIR_PATH}//Аттестационные листы//{file_path}', 'print')
+    #         sleep(10)
