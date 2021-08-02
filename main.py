4af7efb771541dbda733feebd5a0c7e0f5f52381
petr_shirin <p.shirin@agora.ru>
2021-08-02 13:32:42 +0800

    Добавил консольный интерфейс для генерации и печати файлов, вынес все настройки в config.json, добавил минимальный отлов ошибок. Изменил структуру файлов и шаблонов, добавил генерацию exe файла, обновил зависимости. Небольшие правки с изменением формата данных.

--- main.py
+++ main.py
@@ -1,49 +1,35 @@
+import os
+
 import docx
 from typing import Union
 import csv
+import json
+from time import sleep
 
-DIR_PATH = ".\\Послужные карты, для курсантов\\"
-TEMPLATE_ROUTES = {
-    'послужные карты': ['Шаблон.docx'],
-    'справки': ['.\\Шаблоны рядового\\Справка рядового.docx', '.\\Шаблоны сержанта\\Справка сержанта.docx'],
-}
+try:
+    with open('config.json', 'r', encoding='utf-8') as settings_file:
+        settings = json.loads(settings_file.read())
+except FileNotFoundError:
+    print('''Не найден файл настроек config.json
+Он должен находиться рядом с файлом main.exe (main.py)''')
+    sleep(2)
+    exit(-1)
 
-NUMBER_MARK_TO_WORD = {
-    "3": 'удовлетворительно',
-    "4": 'хорошо',
-    "5": 'отлично',
-}
-
-DATA_FILES = ["1_DATA.csv", "2_DATA.csv", "3_DATA.csv"]
-DIR_TO_SAVE = "взвод"
-CERTIFICATION_LIST_TEMPLATES = {
-    'рядовой': {
-        'удовлетворительно': '.\\Шаблоны рядового\\Аттестационный лист рядового удовлетворительно.docx',
-        'хорошо': '.\\Шаблоны рядового\\Аттестационный лист рядового хорошо.docx',
-        'отлично': '.\\Шаблоны рядового\\Аттестационный лист рядового отлично.docx',
-    },
-    'сержант': {
-        'удовлетворительно': '.\\Шаблоны сержанта\\Аттестационный лист сержанта удовлетворительно.docx',
-        'хорошо': '.\\Шаблоны сержанта\\Аттестационный лист сержанта хорошо.docx',
-        'отлично': '.\\Шаблоны сержанта\\Аттестационный лист сержанта отлично.docx',
-    }
-}
-SERVICE_CHARACTERISTIC_TEMPLATES = {
-    'рядовой': {
-        'удовлетворительно': '.\\Шаблоны рядового\\Служебная характеристика рядового удовлетворительно.docx',
-        'хорошо': '.\\Шаблоны рядового\\Служебная характеристика рядового хорошо.docx',
-        'отлично': '.\\Шаблоны рядового\\Служебная характеристика рядового отлично.docx',
-    },
-    'сержант': {
-        'удовлетворительно': '.\\Шаблоны сержанта\\Служебная характеристика сержанта удовлетворительно.docx',
-        'хорошо': '.\\Шаблоны сержанта\\Служебная характеристика сержанта хорошо.docx',
-        'отлично': '.\\Шаблоны сержанта\\Служебная характеристика сержанта отлично.docx',
-    }
-}
+try:
+    DIR_PATH = settings['DIR_PATH']
+    TEMPLATE_ROUTES = settings['TEMPLATE_ROUTES']
+    NUMBER_MARK_TO_WORD = settings['NUMBER_MARK_TO_WORD']
+    DATA_FILES = settings['DATA_FILES']
+    CERTIFICATION_LIST_TEMPLATES = settings['CERTIFICATION_LIST_TEMPLATES']
+    SERVICE_CHARACTERISTIC_TEMPLATES = settings['SERVICE_CHARACTERISTIC_TEMPLATES']
+except KeyError:
+    print('Неверно указан файл настроек config.json')
+    sleep(2)
+    exit(-1)
 
 
-def create_docx_file_by_template(dir_path: str, template_name: str) -> docx.Document:
-    docx_file = docx.Document(dir_path + template_name)
+def create_docx_file_by_template(template_path: str) -> docx.Document:
+    docx_file = docx.Document(template_path)
     return docx_file
 
 
@@ -96,7 +82,7 @@ def update_cell_value(self: docx.document.Document,
 
 
 def show_template_cols():
-    docx_file = docx.Document(DIR_PATH + TEMPLATE_ROUTES['послужные карты'][0])
+    docx_file = docx.Document(TEMPLATE_ROUTES['послужные карты'][0])
     i = 0
     for table in docx_file.tables:
         j = 0
@@ -113,26 +99,30 @@ def show_template_cols():
 
 
 def show_template_paragraphs():
-    docx_file = docx.Document(DIR_PATH + SERVICE_CHARACTERISTIC_TEMPLATES['сержант']['3'])
+    docx_file = docx.Document(SERVICE_CHARACTERISTIC_TEMPLATES['сержант']['3'])
     i = 0
     for par in docx_file.paragraphs:
         print(i, par.text)
         i += 1
 
 
-def create_posluj_cards(template: str, data_file: str = DATA_FILES[0], dir_to_save: str = DIR_TO_SAVE):
+def create_posluj_cards(template: str, data_file: str, dir_to_save: str):
+    remove_data_from_dir(f'{DIR_PATH}{dir_to_save}\\Послужные карты')
+    count = 0
+    try:
+        os.mkdir(f'{DIR_PATH}{dir_to_save}\\Послужные карты')
+    except FileExistsError:
+        pass
     with open(data_file, newline='', encoding='utf-8') as f:
-        reader = csv.DictReader(f, delimiter='|')
+        reader = csv.DictReader(f, delimiter=';')
         for row in reader:
-            docx_file = create_docx_file_by_template(
-                DIR_PATH,
-                template)
+            docx_file = create_docx_file_by_template(template)
             update_cell_value(docx_file, 0, 1, 0, row['ФИО'], 1)
             update_cell_value(docx_file, 0, 2, 0, [row['Дата рождения']], 6)
             update_cell_value(docx_file, 0, 3, 0, row['Место рождения'], 1)
             update_cell_value(docx_file, 0, 4, 0, 'русский', 5)
             update_cell_value(docx_file, 0, 5, 1,
-                              f'ФГБОУ ВО "Иркутский государственный университет", {row["Год_окончания_учебы"]}', 1)
+                              f'ФГБОУ ВО "Иркутский государственный университет", {row["Год окончания учебы"]}', 1)
             update_cell_value(docx_file, 0, 7, 2, [row['Серия'], row['Номер'], row['Дата выдачи'], row['Кем выдан']], 6)
             update_cell_value(docx_file, 0, 12, 1, row['ИНН'], 1)
             update_cell_value(docx_file, 1, 0, 1, row['ВК'], 1)
@@ -147,16 +137,29 @@ def create_posluj_cards(template: str, data_file: str = DATA_FILES[0], dir_to_sa
                                                    row['Оммуниция']], 6)
             update_cell_value(docx_file, 2, 8, 1, row['Фактический адрес'], 1)
             docx_file.save(DIR_PATH + f"{dir_to_save}\\Послужные карты\\Послужная карта {row['ФИО']}.docx")
+            count += 1
+    return count
 
 
-def create_help_cards(template: str, data_file: str = DATA_FILES[0], dir_to_save: str = DIR_TO_SAVE):
+def remove_data_from_dir(dir_to_remove):
+    for walk_position in os.walk(dir_to_remove):
+        for file in walk_position[2]:
+            if '.docx' in file:
+                os.remove(f'{dir_to_remove}\\{file}')
+
+
+def create_help_cards(template: str, data_file: str, dir_to_save: str):
+    remove_data_from_dir(f'{DIR_PATH}{dir_to_save}\\Справки')
+    count = 0
+    try:
+        os.mkdir(f'{DIR_PATH}{dir_to_save}\\Справки')
+    except FileExistsError:
+        pass
     with open(data_file, newline='', encoding='utf-8') as f:
-        reader = csv.DictReader(f, delimiter='|')
+        reader = csv.DictReader(f, delimiter=';')
         for row in reader:
-            docx_file = create_docx_file_by_template(
-                DIR_PATH,
-                template)
-            values = [row['ФИОдат'], row['ВТП'], row['Итог']]
+            docx_file = create_docx_file_by_template(template)
+            values = [row['ФИО в дательном'], NUMBER_MARK_TO_WORD[row['ВТП']], NUMBER_MARK_TO_WORD[row['Итог']]]
             i = 0
             for paragraph in docx_file.paragraphs:
                 if i > len(values):
@@ -171,19 +174,25 @@ def create_help_cards(template: str, data_file: str = DATA_FILES[0], dir_to_save
                     print(values)
                     raise e
             docx_file.save(DIR_PATH + f"{dir_to_save}\\Справки\\Справка {row['ФИО']}.docx")
+            count += 1
+    return count
 
 
-def create_service_characteristics(template: dict, data_file: str = DATA_FILES[0], dir_to_save: str = DIR_TO_SAVE):
+def create_service_characteristics(template: dict, data_file: str, dir_to_save: str):
+    remove_data_from_dir(f'{DIR_PATH}{dir_to_save}\\Служебные характеристики')
+    count = 0
+    try:
+        os.mkdir(f'{DIR_PATH}{dir_to_save}\\Служебные характеристики')
+    except FileExistsError:
+        pass
     with open(data_file, newline='', encoding='utf-8') as f:
-        reader = csv.DictReader(f, delimiter='|')
+        reader = csv.DictReader(f, delimiter=';')
         for row in reader:
             try:
-                docx_file = create_docx_file_by_template(
-                    DIR_PATH,
-                    template[row['Итог']])
+                docx_file = create_docx_file_by_template(template[row['Итог']])
             except KeyError:
                 continue
-            values = [row['ФИОрод'], row['ФИО']]
+            values = [row['ФИО в родительном'], row['ФИО']]
             i = 0
             for paragraph in docx_file.paragraphs:
                 if i > len(values):
@@ -199,21 +208,30 @@ def create_service_characteristics(template: dict, data_file: str = DATA_FILES[0
                     raise e
             docx_file.save(
                 DIR_PATH + f"{dir_to_save}\\Служебные характеристики\\Служебная характеристика {row['ФИО']}.docx")
+            count += 1
+    return count
 
 
-def create_certifications_lists(template: dict, data_file: str = DATA_FILES[0], dir_to_save: str = DIR_TO_SAVE):
+def create_certifications_lists(template: dict, data_file: str, dir_to_save: str):
+    remove_data_from_dir(f'{DIR_PATH}{dir_to_save}\\Аттестационные листы')
+    count = 0
+    try:
+        os.mkdir(f'{DIR_PATH}{dir_to_save}\\Аттестационные листы')
+    except FileExistsError:
+        pass
     with open(data_file, newline='', encoding='utf-8') as f:
-        reader = csv.DictReader(f, delimiter='|')
+        reader = csv.DictReader(f, delimiter=';')
         for row in reader:
-            docx_file = create_docx_file_by_template(
-                DIR_PATH,
-                template.get(row['Итог'], template['удовлетворительно']))
-            values = [row['ФИОдат'],
+            try:
+                docx_file = create_docx_file_by_template(template[row['Итог']])
+            except KeyError:
+                continue
+            values = [row['ФИО в дательном'],
                       row['Дата рождения'],
                       row['Место рождения'],
                       row['Факультет'],
-                      row['Год_окончания_учебы'], 
-                      row['Гр_специальность']]
+                      row['Год окончания учебы'],
+                      row['Гражданская специальность']]
             i = 0
             for paragraph in docx_file.paragraphs:
                 if i > len(values):
@@ -228,21 +246,58 @@ def create_certifications_lists(template: dict, data_file: str = DATA_FILES[0], 
                     print(values)
                     raise e
             docx_file.save(DIR_PATH + f"{dir_to_save}\\Аттестационные листы\\Аттестационный лист {row['ФИО']}.docx")
+            count += 1
+    return count
 
 
 if __name__ == '__main__':
-    create_posluj_cards(TEMPLATE_ROUTES['послужные карты'][0], DATA_FILES[0], '1 взвод')
-    create_posluj_cards(TEMPLATE_ROUTES['послужные карты'][0], DATA_FILES[1], '2 взвод')
-    create_posluj_cards(TEMPLATE_ROUTES['послужные карты'][0], DATA_FILES[2], '3 взвод')
+    generate_variant = 5
+    count_generated_documents = 0
+    try:
+        generate_variant = int(input('''Что хотите сгенерировать?
+1 - Послужные карты
+2 - Справки
+3 - Аттестационные листы
+4 - Служебные характеристики
+5 - Все документы
+Вариант генерации документов: '''))
+    except ValueError:
+        print('Ошибка в формате')
+        sleep(2)
+        exit(-1)
+    for key, value in DATA_FILES['сержант'].items():
+        try:
+            os.mkdir(f'{DIR_PATH}{key} взвод')
+        except FileExistsError:
+            pass
+        if generate_variant == 1 or generate_variant == 5:
+            count_generated_documents += create_posluj_cards(TEMPLATE_ROUTES['послужные карты']['сержант'], value,
+                                                             f'{key} взвод')
+        if generate_variant == 2 or generate_variant == 5:
+            count_generated_documents += create_help_cards(TEMPLATE_ROUTES['справки']['сержант'], value, f'{key} взвод')
+        if generate_variant == 3 or generate_variant == 5:
+            count_generated_documents += create_certifications_lists(CERTIFICATION_LIST_TEMPLATES['сержант'], value,
+                                                                     f'{key} взвод')
+        if generate_variant == 4 or generate_variant == 5:
+            count_generated_documents += create_service_characteristics(SERVICE_CHARACTERISTIC_TEMPLATES['сержант'],
+                                                                        value, f'{key} взвод')
 
-    create_help_cards(TEMPLATE_ROUTES['справки'][1], DATA_FILES[0], '1 взвод')
-    create_help_cards(TEMPLATE_ROUTES['справки'][0], DATA_FILES[1], '2 взвод')
-    create_help_cards(TEMPLATE_ROUTES['справки'][0], DATA_FILES[2], '3 взвод')
+    for key, value in DATA_FILES['рядовой'].items():
+        try:
+            os.mkdir(f'{DIR_PATH}{key} взвод')
+        except FileExistsError:
+            pass
+        if generate_variant == 1 or generate_variant == 5:
+            count_generated_documents += create_posluj_cards(TEMPLATE_ROUTES['послужные карты']['рядовой'], value,
+                                                             f'{key} взвод')
+        if generate_variant == 2 or generate_variant == 5:
+            count_generated_documents += create_help_cards(TEMPLATE_ROUTES['справки']['рядовой'], value, f'{key} взвод')
+        if generate_variant == 3 or generate_variant == 5:
+            count_generated_documents += create_certifications_lists(CERTIFICATION_LIST_TEMPLATES['сержант'], value,
+                                                                     f'{key} взвод')
+        if generate_variant == 4 or generate_variant == 5:
+            count_generated_documents += create_service_characteristics(SERVICE_CHARACTERISTIC_TEMPLATES['сержант'],
+                                                                        value, f'{key} взвод')
 
-    create_certifications_lists(CERTIFICATION_LIST_TEMPLATES['сержант'], DATA_FILES[0], '1 взвод')
-    create_certifications_lists(CERTIFICATION_LIST_TEMPLATES['рядовой'], DATA_FILES[1], '2 взвод')
-    create_certifications_lists(CERTIFICATION_LIST_TEMPLATES['рядовой'], DATA_FILES[2], '3 взвод')
-
-    create_service_characteristics(SERVICE_CHARACTERISTIC_TEMPLATES['сержант'], DATA_FILES[0], '1 взвод')
-    create_service_characteristics(SERVICE_CHARACTERISTIC_TEMPLATES['рядовой'], DATA_FILES[1], '2 взвод')
-    create_service_characteristics(SERVICE_CHARACTERISTIC_TEMPLATES['рядовой'], DATA_FILES[2], '3 взвод')
+    print(f'Генерация документов завершена, сгенерированно {count_generated_documents} файлов')
+    sleep(5)
--- print_files.py
+++ print_files.py
@@ -1,31 +1,59 @@
 import os
 from time import sleep
+import json
 
-DIR_PATH = '.\\Послужные карты, для курсантов\\2 взвод'
+try:
+    with open('config.json', 'r', encoding='utf-8') as settings_file:
+        settings = json.loads(settings_file.read())
+except FileNotFoundError:
+    print('''Не найден файл настроек config.json
+Он должен находиться рядом с файлом print_files.exe (print_files.py)''')
+    sleep(2)
+    exit(-1)
+
+try:
+    DIR_PATH = settings['DIR_PATH']
+except KeyError:
+    print('Неверно указан файл настроек config.json')
+    sleep(2)
+    exit(-1)
 
 if __name__ == '__main__':
+    doc_to_print = None
+    count_to_print = 0
+    group = 0
+    try:
+        doc_to_print = int(input('''Какие документы вы хотите распечатать?
+1 - Служебные характеристики
+2 - Справки
+3 - Аттестационные листы
+Документ на печать: '''))
+        count_to_print = int(input('''Количество экземпляров на печать: '''))
+        group = int(input('''Выберите номер взвода(пример: 1): '''))
+    except ValueError:
+        print("Ошибка ввода данных")
+        sleep(2)
+        exit(-1)
     file_names = []
-    # for f in os.walk(DIR_PATH + '\\Служебные характеристики'):
-    #     file_names = f[2]
-    # for file_path in file_names:
-    #     if '.docx' in file_path:
-    #         os.startfile(f'{DIR_PATH}\\Служебные характеристики\\{file_path}', 'print')
-    #         sleep(10)
-    #         os.startfile(f'{DIR_PATH}\\Служебные характеристики\\{file_path}', 'print')
-    #         sleep(10)
-    # for f in os.walk(DIR_PATH + '\\Справки'):
-    #     file_names = f[2]
-    # for file_path in file_names:
-    #     if '.docx' in file_path:
-    #         os.startfile(f'{DIR_PATH}\\Справки\\{file_path}', 'print')
-    #         sleep(10)
-    #         os.startfile(f'{DIR_PATH}\\Справки\\{file_path}', 'print')
-    #         sleep(10)
-    #         os.startfile(f'{DIR_PATH}\\Справки\\{file_path}', 'print')
-    #         sleep(10)
-    for f in os.walk(DIR_PATH + '\\Аттестационные листы'):
+    path_to_walk = None
+    if doc_to_print == 1:
+        path_to_walk = DIR_PATH + f'\\{group} взвод\\Служебные характеристики'
+    elif doc_to_print == 2:
+        path_to_walk = DIR_PATH + f'\\{group} взвод\\Справки'
+    elif doc_to_print == 3:
+        path_to_walk = DIR_PATH + f'\\{group} взвод\\Аттестационные листы'
+    else:
+        print("Ошибка выбора типа документов")
+        sleep(2)
+        exit(-1)
+    for f in os.walk(path_to_walk):
         file_names = f[2]
     for file_path in file_names:
         if '.docx' in file_path:
-            os.startfile(f'{DIR_PATH}\\Аттестационные листы\\{file_path}', 'print')
-            sleep(10)
+            for i in range(count_to_print):
+                print(f'Отправляю на печать файл {file_path}')
+                os.startfile(f'{path_to_walk}\\{file_path}', 'print')
+                sleep(7)
+
+    print('Все файлы были отправлены на печать')
+    sleep(5)
